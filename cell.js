cafd2143a7f6fcf9ac983fdfd77c5686bb63b4a2
Théo Sabattié <theo.sabattie@gmail.com>
2017-07-05 17:48:59 +0200

    Indentation to space

--- cell.js
+++ cell.js
@@ -1,16 +1,16 @@
 function Cell(x, y, id, content){
-	this.x 		 = x;
-	this.y 		 = y;
-	this.id 	 = id;
-	this.content = content;
+    this.x       = x;
+    this.y       = y;
+    this.id      = id;
+    this.content = content;
 }
 
 Cell.prototype.getValue = function (){
-	if (!this.content){
-		return null;
-	}
+    if (!this.content){
+        return null;
+    }
 
-	return this.content.v;
+    return this.content.v;
 };
 
 module.exports = Cell;
